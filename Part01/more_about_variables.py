2958f7a788c95950479e09c84cbe765addadec23
Kocatas <kocatasefe@gmail.com>
2022-05-04 20:59:51 +0300

    Last Form

---
+++ Part01/more_about_variables.py
@@ -0,0 +1,13 @@
+#declaring integer type variables
+
+age = 30
+star = 5
+string_variable = "John Doe"
+
+#strings and integers
+
+my_variable = 30
+my_string = "100"
+
+print(my_string + " " + str(my_variable)) #In here, str() method is changing string data type into integer one.
+
