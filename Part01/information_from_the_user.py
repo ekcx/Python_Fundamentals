8342401d07041990cb731e763663324b95ce4abc
Kocatas <kocatasefe@gmail.com>
2022-05-04 20:48:34 +0300

    Last Form

--- Part01/information_from_the_user.py
+++ Part01/information_from_the_user.py
@@ -11,3 +11,16 @@ print(age * 3)
 
 name2 = input("Your name: ")
 print("Your name " + name2 + " is String!")
+
+first_name = input("What is your name?")
+email = input("What is your email address?")
+nickname = input("What is your nick name?")
+
+print("Let's make sure make this right!")
+print("Your name:" + " " + first_name)
+print("Your mail address:" + " " + email)
+print("Your Nickname:" + " " + nickname)
+
+#We know that if any variable is declared more than one, its value is the last entered one!
+
+
