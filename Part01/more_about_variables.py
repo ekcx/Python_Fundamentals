9c8ebfdcd218912bef4b49ea87afa9b8a34e70a5
Kocatas <kocatasefe@gmail.com>
2022-05-04 21:04:00 +0300

    Last Form

--- Part01/more_about_variables.py
+++ Part01/more_about_variables.py
@@ -11,3 +11,9 @@ my_string = "100"
 
 print(my_string + " " + str(my_variable)) #In here, str() method is changing string data type into integer one.
 
+# Printing f-strings
+# This is a sexy feature of Python.
+
+# f-string structure depends on the changing string data type to integer with a special f" {string_variable}"
+print(f"my_variable is {my_variable}")
+
