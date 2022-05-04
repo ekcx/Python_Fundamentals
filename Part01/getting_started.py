fc99fffe7d12ea66754b0dbd09abfee734c6aaa3
Kocatas <kocatasefe@gmail.com>
2022-05-04 20:17:03 +0300

    Initial commit

---
+++ Part01/getting_started.py
@@ -0,0 +1,24 @@
+print("Say something!")
+print("Display this way.")
+
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = "Kocatas"
+g = "Efe"
+print(a)
+print(f + " " + g)
+print(2*3*4)
+
+print("efe"*3)
+#will be displaying -> efeefeefe
+
+print("MyName"*5)
+#will be displayin -> MyNname as 5 times
+# This is the multiplication of string and integer
+
+print("5 * 4 * 3 + 7")
+print('"Come Back right now!!", said the police officer!')
+#The importance of Quotation Mark
