275592651e3889db554bff2eb7fe337a7c4c15b7
Kocatas <kocatasefe@gmail.com>
2022-05-04 20:29:57 +0300

    New File

---
+++ Part01/information_from_the_user.py
@@ -0,0 +1,12 @@
+name = input("Please enter your name: ")
+print("Hi there, " + name)
+
+age = input("Please enter your age: ")
+print("Your age is : " + age)
+
+print(age * 3)
+
+# Please attention! "input" method is taking the string always!
+# data type of any variable by taking input is STRING !!!!
+
+
