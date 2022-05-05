0ff88344d2ce3f5cf6363dfec287a7aba25e1f29
Kocatas <kocatasefe@gmail.com>
2022-05-05 12:24:01 +0300

    Updated Files

--- Python_Exercises/exercise0001.py
+++ Python_Exercises/exercise0001.py
@@ -1,5 +1,7 @@
 degree = int(input("Please enter the fahrenheit: "))
 # (32°F − 32) × 5/9 = 0°C
+print(str(degree) + " Fahrenheit is  ")
 degree = (degree - 32) * (5/9)
-print(degree)
+print(str(degree) + " degree")
+
 
