8198c7f9bbf268a7105a4cb58ffc1ad38ab47aa6
Kocatas <kocatasefe@gmail.com>
2022-05-05 10:10:06 +0300

    Updated Files

---
+++ Part01/arithmetic_operations.py
@@ -0,0 +1,14 @@
+height = 172.5
+weight = 68.55
+bmi = weight / (height / 100) ** 2
+print(f"The BMI is {bmi}")
+
+# Python has a another feature that divides and round down nearest integer by using double slash //
+
+x = 10
+y = 4.2
+z = x // y # will be display 2
+print(z)
+z = x / y # will be display floating point number.
+print(z)
+
