ac1054e5bfe500d198d07868b8d29efb32cacf10
Kocatas <kocatasefe@gmail.com>
2022-05-05 15:22:35 +0300

    Updated Files

--- Part02/programming_terminology.py
+++ Part02/programming_terminology.py
@@ -10,4 +10,35 @@ if age > 17:
 print("This is the another block.")
 
 # The indentation should be the same for the same blocks.
+# Any python programs should be started the leftmost edge. Otherwise, it will not operate.
+
+# To find the data type, we can use type() method.
+
+name = "Anna"
+result = 100
+
+print(type(name))
+print(type(result))
+
+# Finding the lenght of the string wih "len"
+
+string_length = input("Please enter the string: ")
+print("The number of lenght of the string: " + str(len(string_length)))
+
+length_variable = len(string_length)
+print(length_variable)
+
+# Empty Strings will display zero.
+# The string is empty.
+
+empty = ""
+length_variable = len(empty)
+print(length_variable)
+
+# Type casting in Python
+
+casting = float(input("Please enter a float number: "))
+
+print(casting)
+print(int(casting))
 
