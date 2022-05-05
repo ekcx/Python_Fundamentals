b9eb6911f7056fceb01d71390b36ddec7cab09e0
Kocatas <kocatasefe@gmail.com>
2022-05-05 13:05:05 +0300

    Updated Files

---
+++ Part02/programming_terminology.py
@@ -0,0 +1,13 @@
+# Block
+age = 18
+if age > 17:
+    # beginning of the conditional block.
+    print("You are of age!")
+    age += 1
+    print("You are now one year older...")
+    # end of the conditional block.
+
+print("This is the another block.")
+
+# The indentation should be the same for the same blocks.
+
