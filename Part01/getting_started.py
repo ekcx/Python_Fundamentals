d94c03e1f9d7f462a9eb885aa65695aa48f824c9
Kocatas <kocatasefe@gmail.com>
2022-05-05 10:00:47 +0300

    Updated Files

--- Part01/getting_started.py
+++ Part01/getting_started.py
@@ -6,8 +6,8 @@ b = 2
 c = 3
 d = 4
 e = 5
-f = "Kocatas"
-g = "Efe"
+f = "Doe"
+g = "John"
 print(a)
 print(f + " " + g)
 print(2*3*4)
@@ -16,7 +16,7 @@ print("efe"*3)
 #will be displaying -> efeefeefe
 
 print("MyName"*5)
-#will be displayin -> MyNname as 5 times
+#will be displaying -> MyNname as 5 times
 # This is the multiplication of string and integer
 
 print("5 * 4 * 3 + 7")
--- Part01/more_about_variables.py
+++ Part01/more_about_variables.py
@@ -17,3 +17,21 @@ print(my_string + " " + str(my_variable)) #In here, str() method is changing str
 # f-string structure depends on the changing string data type to integer with a special f" {string_variable}"
 print(f"my_variable is {my_variable}")
 
+# Examples f-string structure
+
+# first declare integer data type.
+
+integer1 = 20
+print(f"This is the f-string structure: {integer1}")
+
+integer2 = 10
+print(f"This is the f-string structure: {integer2}")
+
+# f-string structure provides using integer data type and string data type together.
+# f-string is equivalent with str() method like this:
+
+print("This is the str(): " + str(integer2))
+print("This is the type casting by using str(): " + str(integer1))
+
+# f-string's are easiest way to work with different data types in Python.
+
