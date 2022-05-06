2ede94a290afadc43622d43f27eb5d4317bfce59
Kocatas <kocatasefe@gmail.com>
2022-05-07 01:13:35 +0300

    Updated Files

--- Part02/more_conditional.py
+++ Part02/more_conditional.py
@@ -54,6 +54,20 @@ else:
     print("It's a tie!")
 
 
+print("Holiday Calendar")
+date = input("What is the date today? ")
+
+if date == "Dec 26":
+    print("It's Boxing Day")
+elif date == "Dec 31":
+    print("It's Hogmanay")
+elif date == "Jan 1":
+    print("It's New Year's Day!")
+
+print("Thanks and Bye.")
+
+
+
 
 
 
