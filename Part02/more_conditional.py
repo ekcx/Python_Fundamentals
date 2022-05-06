557c7ed875ef3c1c255ec1a0ce507a1543bdd5a4
Kocatas <kocatasefe@gmail.com>
2022-05-07 01:18:28 +0300

    Updated Files

--- Part02/more_conditional.py
+++ Part02/more_conditional.py
@@ -29,7 +29,7 @@ else:
 # Another example with string comparison
 
 correct = "kittycat"
-password = input("Please enter a passowrd: ")
+password = input("Please enter a password: ")
 
 if password == correct:
     print("Correct!")
@@ -65,6 +65,94 @@ elif date == "Jan 1":
     print("It's New Year's Day!")
 
 print("Thanks and Bye.")
+
+number1 = int(input("Please type in the first number:"))
+number2 = int(input("Please type in another number:"))
+
+if number1 == number2:
+    print("The numbers are equal!")
+elif number1 > number2:
+    print(f"The greater number was: {number1}")
+else:
+    print(f"The greater number was: {number2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
