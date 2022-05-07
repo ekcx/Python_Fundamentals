22aaa3a235e68a2c1e238f66b80e903de6c1e36d
Kocatas <kocatasefe@gmail.com>
2022-05-08 01:02:09 +0300

    Updated Files

--- Python_Exercises/exercise0002.py
+++ Python_Exercises/exercise0002.py
@@ -13,3 +13,44 @@ while True:
         helper_string += string + " "
 
 print(helper_string)
+
+var = ""
+sentence = ""
+
+while sentence != "end":
+    sentence = input("Type any word: ")
+
+    if sentence != "end":
+        var += sentence + " "
+    else:
+        break
+
+print(var)
+
+# Mean, Summation, Count, Positive-Negative
+
+number = 1
+summation = 0
+count = 0
+positive_number = 0
+negative_number = 0
+
+
+while number != 0:
+    number = int(input("Type a number: "))
+    summation += number
+
+    if number != 0:
+        count += 1
+        if number > 0:
+            positive_number += 1
+        else:
+            negative_number += 1
+    else:
+        break
+
+print("\n")
+print(f"The summation is {summation}")
+print(f"The mean is {summation / count}")
+print(f"The positive number is {positive_number}")
+print(f"The negative number is {negative_number}")
