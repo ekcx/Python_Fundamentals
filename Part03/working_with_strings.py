21c3da721891285a7949d868c00392bb23211d67
Kocatas <kocatasefe@gmail.com>
2022-05-13 02:24:13 +0300

    Updated Files

--- Part03/working_with_strings.py
+++ Part03/working_with_strings.py
@@ -32,4 +32,38 @@ elif len(compare_string2) > len(compare_string1):
 else:
     print("Each strings are equal to each other.", compare_string1, "=", compare_string2)
 
+get_str = input("Please enter a string: ")
+print(get_str[0])
+print(get_str[1])
+print(get_str[5])
+
+print("The firs character of the string:", get_str[0])
+print("The last character of the string:", get_str[len(get_str)-1])
+
+# There is a negative index in Python.
+
+get_str = input("Type in a string: ")
+j = 0
+
+# This will be displaying in reverse order.
+
+for i in range(1, len(get_str)+1):
+    j = i * -1
+    print(get_str[j])
+
+# -1 is the last index of the string
+
+testing = input("Type in a string: ")
+print("First Character: ", testing[0])
+print("Last Character: ", testing[-1])
+
+get_str = input("Type a string: ")
+
+if get_str[2] == get_str[-1]:
+    print("The second and the last character is the same: ", get_str[2])
+else:
+    print("The second and the last character is different.")
+
+get_number = int(input("Width:"))
+print("#" * get_number)
 
