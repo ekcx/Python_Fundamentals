a0cd1992b4c1dc863f11ef591fff3d374c2f2acd
Kocatas <kocatasefe@gmail.com>
2022-05-13 12:05:51 +0300

    Updated Files

--- Part03/strings_exercises.py
+++ Part03/strings_exercises.py
@@ -15,3 +15,17 @@ else:
 print(string[:5])  # starting default 0
 print(string[3:])  # ending default 0
 
+# [a:b] is half-open, a included, b excluded
+
+get_string = input("Type in a string:")
+lenght = len(get_string)
+
+for i in range(0, lenght):
+    print(get_string[i:lenght])
+
+for i in range(lenght, -1, -1):
+    print(get_string[i:lenght])
+
+for i in range(0, lenght):
+    print(get_string[0:i+1])
+
