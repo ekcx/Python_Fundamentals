7673ed8ba549dcf7a0e3f5e9fd176e043ca36599
Kocatas <kocatasefe@gmail.com>
2022-06-25 21:29:53 +0300

    Updated Files

---
+++ Part03/defining_functions.py
@@ -0,0 +1,23 @@
+# Defining Function
+
+def my_function():
+    print("This is my first function!")
+
+# After class or function, we have minimum 2 blank line to continue writing.
+
+my_function()
+
+
+def summation(x, y):
+    print(x + y)
+
+
+summation(5, 10)
+
+
+def multiplication(x, y):
+    print(x * y)
+
+
+multiplication(10, 21)
+
