6702da946bb79adbe94270cdcb0fca1ceb7fd98e
Kocatas <kocatasefe@gmail.com>
2022-06-25 12:37:20 +0300

    Updated Files

--- Part03/working_with_strings3.py
+++ Part03/working_with_strings3.py
@@ -1,13 +1,24 @@
+# Character Occurence Control
+import gettext
 
-main_string = input("Enter the string: ")
-occurence_string = input("Enter the substring: ")
+main_string = input("Type in a string: ")
+occurence_character = input("Type in a character: ")
 string_check = 0
 
-while True:
-    if occurence_string in main_string:
-        string_check = 1
+for i in main_string:
+    if i == occurence_character:
+        string_check = string_check + 1
 
-    if main_string.find(occurence_string) != -1 and string_check == 1:
-        print(main_string.find(string_check))
-    else:
-        print("The second occurence of the substring is not here.")
+print(string_check)
+
+# The Second Occurence
+
+occurence = input("Please enter the string: ")
+substring = input("Type a substring: ")
+
+firstText = occurence.find(substring)
+
+if occurence.find(substring, firstText + 1) != -1:
+    print("The second occurence of the substring is at index", occurence.find(substring, firstText + 1))
+else:
+    print("The string has no second substring.")
